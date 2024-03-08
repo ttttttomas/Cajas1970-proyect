f5b039086b0141127a829ee1a0368d9de30fec3f
Tomas <totobarajas124@gmail.com>
2024-03-08 18:19:30 -0300

    add products nav

--- app.js
+++ app.js
@@ -2,9 +2,15 @@ const arrow = document.getElementById("arrow");
 const nav = document.getElementById("nav");
 const letters = document.getElementById("letters");
 const text = document.getElementById("divText");
+const menu = document.getElementById("menu");
+const header = document.getElementById("header");
+const products = document.getElementById("products");
 
 
 arrow.addEventListener("click",()=>{
     nav.classList.toggle("close");
-    text.classList.toggle("close")
+    text.classList.toggle("close");
+    
 })
+
+console.log(products)
