7cedbf9e04ed3fe20a4f41069a3ef820892aa149
Tomas <totobarajas124@gmail.com>
2024-03-08 15:38:47 -0300

    add app.js, fix nav

---
+++ app.js
@@ -0,0 +1,12 @@
+const arrow = document.getElementById("arrow");
+const nav = document.getElementById("nav");
+const letters = document.getElementById("letters");
+const text = document.getElementById("divText");
+
+
+arrow.addEventListener("click",()=>{
+    nav.classList.toggle("close");
+    text.classList.toggle("close")
+})
+
+console.log(text)
