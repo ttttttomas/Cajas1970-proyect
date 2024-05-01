0ee7fcfb52b2f89a9806fa48a4d1e4ad4dfe2ef6
Tomas <totobarajas124@gmail.com>
2024-05-01 15:23:52 -0300

    add products.html

--- app.js
+++ app.js
@@ -1,19 +1,12 @@
-const arrow = document.getElementById("arrow");
-const nav = document.getElementById("nav");
-const letters = document.getElementById("letters");
-const text = document.getElementById("divText");
-const menu = document.getElementById("menu");
-const header = document.getElementById("header");
-const products = document.getElementById("products");
-const arrowList = document.querySelector(".nav .container-letters .bx");
-const containerList = document.querySelector(".nav .list-inside");
-const sucursales = document.querySelector(".nav .sucursales-container");
+const dom = (element)=>{
+return document.getElementById(element)
+}
 
+let products = [];
 
-arrowList.addEventListener("click",()=>{
-    containerList.classList.toggle("close");
-} )
-
-
-
-
+fetch("./products.json")
+    .then(response => response.json())
+    .then(data => {
+        products = data;
+        cargarProductos(products);
+    })
