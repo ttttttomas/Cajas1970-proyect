a7871c6836edfcd807b1713151d83cc62e827949
Tomas <totobarajas124@gmail.com>
2024-06-21 13:37:47 -0300

    update images

--- app.js
+++ app.js
@@ -2,11 +2,31 @@ const dom = (element)=>{
 return document.getElementById(element)
 }
 
-let products = [];
+let products = []
 
-fetch("./products.json")
-    .then(response => response.json())
-    .then(data => {
-        products = data;
-        cargarProductos(products);
-    })
+fetch('./products.json')
+  .then(response => response.json())
+  .then(data => {
+    products = data;
+    (uploadProducts(products))
+  });
+
+const divProducts = dom("box-products-wrapper")
+console.log(divProducts)
+
+
+const uploadProducts = () => {
+
+    products.map(product=>{
+
+        const div = document.createElement("div");
+        div.classList.add("box-product")
+        div.innerHTML = `
+        <img src="${product.img}" alt="${product.title}">
+        <p class="title">${product.title}</p>
+        <p class="medida">${product.medida}</p>
+        `
+        divProducts.append(div);
+})
+}
+uploadProducts()
