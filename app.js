029c20d0ba93d620ddccb7d9068315d0660b60e4
Tomas <totobarajas124@gmail.com>
2024-06-29 17:12:08 -0300

    fixed box medidas, adding products

--- app.js
+++ app.js
@@ -7,93 +7,173 @@ const btns = document.querySelectorAll(".filters")
 
 let products = [
   {
+    "img": "./img/fotoscajas/maletin/maletin 5.png",
+    "title": "Budinera",
+    "medida": "23x8.5x8.5 | 24x10x10",
+    "categoria": "box",
+    "id" : "BOX"
+  },
+  {
       "img": "./img/fotoscajas/BOX/box.png",
-      "title": "BOX",
+      "title": "Box",
       "medida": "19x12x9 | 24x16x10 | 24x14x12",
       "categoria": "box",
       "id" : "BOX"
     },
     {
       "img": "./img/fotoscajas/BOX/box 20x20x10.png",
-      "title": "BOX",
+      "title": "Box",
       "medida": "20x20x10",
       "categoria": "box",
       "id" : "BOX"
     },
     {
       "img": "./img/fotoscajas/BOX/box 25x12x7.png",
-      "title": "BOX",
-      "medida": "25x20x7 | 25x12x7",
+      "title": "Box",
+      "medida": "25x20x7",
       "categoria": "box",
       "id" : "BOX"
     },
     {
-      "img": "./img/fotoscajas/BOX/Box ",
-      "title": "Frank Herbert",
-      "medida": "1965",
+      "img": "./img/fotoscajas/tapa y caja/Cajatapa1.png",
+      "title": "Tapa y caja",
+      "medida": "25x25x10 | 25x25x15 | 32x25x10",
       "categoria": "box",
-      "id" : "MACARRON"
+      "id" : "TAPA Y CAJA"
     },
     {
-      "img": "D",
-      "title": "Frank Herbert",
-      "medida": "1965",
+      "img": "./img/fotoscajas/tapa y caja/tapacaja2.png",
+      "title": "Tapa y caja",
+      "medida": "25x25x10 | 25x25x15 | 32x25x10",
+      "categoria": "box",
+      "id" : "TAPA Y CAJA"
+    },
+    {
+      "img": "./img/fotoscajas/tapa y caja/tapacaja3.png",
+      "title": "Tapa y caja",
+      "medida": "25x25x10 | 25x25x15 | 32x25x10",
+      "categoria": "box",
+      "id" : "TAPA Y CAJA"
+    },
+    {
+      "img": "./img/fotoscajas/desayuno/desayuno2.png",
+      "title": "Tapa y caja",
+      "medida": "25x25x10 | 25x25x15 | 32x25x10",
+      "categoria": "box",
+      "id" : "DESAYUNO"
+    },
+    {
+      "img": "./img/fotoscajas/desayuno/desayuno1.png",
+      "title": "Desayuno",
+      "medida": "25x25x12 | 30x30x12 | 36x26x12 | 42x32x12 | HEXAGONAL Ø 8",
+      "categoria": "box",
+      "id" : "DESAYUNO"
+    },
+    {
+      "img": "./img/fotoscajas/desayuno/desayuno4.png",
+      "title": "Desayuno",
+      "medida": "25x25x12 | 30x30x12 | 36x26x12 | 42x32x12 | HEXAGONAL Ø 8",
+      "categoria": "box",
+      "id" : "DESAYUNO"
+    },
+    {
+      "img": "./img/fotoscajas/deslizable/deslizable.png",
+      "title": "Deslizables",
+      "medida": "16x16x4 | 20x20x4 | 25x25x7 | 30x30x7",
+      "categoria": "box",
+      "id" : "DESLIZABLES"
+    },
+    {
+      "img": "./img/fotoscajas/deslizable/Deslizables 1.png",
+      "title": "Deslizables",
+      "medida": "16x16x4 | 20x20x4 | 25x25x7 | 30x30x7",
+      "categoria": "box",
+      "id" : "DESLIZABLES"
+    },
+    {
+      "img": "./img/fotoscajas/deslizable/deslizables2.png",
+      "title": "Deslizables",
+      "medida": "16x16x4 | 20x20x4 | 25x25x7 | 30x30x7",
+      "categoria": "box",
+      "id" : "DESLIZABLES"
+    },
+    {
+      "img": "./img/fotoscajas/torta/torta1.png",
+      "title": "Torta maletin",
+      "medida": "16x16x4 | 20x20x4 | 25x25x7 | 30x30x7",
       "categoria": "box",
       "id" : "TORTA"
     },
     {
-      "img": "D",
-      "title": "Frank Herbert",
-      "medida": "1965",
+      "img": "./img/fotoscajas/torta/torta2.png",
+      "title": "Torta maletin",
+      "medida": "16x16x4 | 20x20x4 | 25x25x7 | 30x30x7",
       "categoria": "box",
       "id" : "TORTA"
     },
     {
-      "img": "D",
-      "title": "Frank Herbert",
-      "medida": "1965",
-      "categoria": "box",
-      "id" : "MALETINES"
+      
     },
     {
-      "img": "D",
-      "title": "Frank Herbert",
-      "medida": "1965",
-      "categoria": "box",
-      "id" : "MALETINES"
-    },
-    {
-      "img": "D",
-      "title": "Frank Herbert",
-      "medida": "1965",
+      "img": "./img/TRANSPARENTE BG.png",
+      "title": "",
+      "medida": "16x16x15 | 16x16x20 | 20x20x15 | 20x20x20 | 25x25x15 </br> 25x25x25 | 25x25x40 | 30x30x15 | 30x30x25 | 30x30x40",
       "categoria": "box",
       "id" : "DRIPS"
     },
     {
-      "img": "D",
-      "title": "Frank Herbert",
-      "medida": "1965",
-      "categoria": "box",
-      "id" : "CUPCAKE"
+      "img": "./img/fotoscajas/DRIP/Drip 3.png",
+      "title": "DRIPS TAPA TRANSPARENTE",
+      "medida": "",
+      "categoria": "DRIPS",
+      "id" : "DRIPS"
     },
+    {
+      "img": "./img/fotoscajas/DRIP/Drip 2.png",
+      "title": "DRIPS EXAGONALES",
+      "medida": "28X25 | 28X40",
+      "categoria": "DRIPS",
+      "id" : "DRIPS"
+    },
+    {
+      "img": "./img/fotoscajas/DRIP/Drips.png",
+      "title": "DRIPS",
+      "medida": "",
+      "categoria": "box",
+      "id" : "DRIPS"
+    }
 ]
+
+const productosFiltradosEnTodos = products.filter(product =>
+  product.medida.length <= 57) 
+
 const uploadProducts = (productosElegidos) => {
 
     divProducts.innerHTML = ""
 
     productosElegidos.map(product=>{
-
-        const div = document.createElement("div");
-        div.classList.add("box-product")
+      
+        let div = document.createElement("div");
+        div.classList.add(`box-product`)
+        div.classList.add()
         div.innerHTML = `
         <img src="${product.img}" alt="${product.title}">
         <p class="title">${product.title}</p>
         <p class="medida">${product.medida}</p>
         `
         divProducts.append(div);
+
+        if (product.medida.length > 57){
+
+          div.classList.add("box-product-drip")
+          div.innerHTML = `
+          <p class="title">MEDIDAS DE DRIP</p>
+            ${product.title} </br>
+            ${product.medida}`
+          divProducts.append(div);
+
+        }
 })
-}
-uploadProducts(products)
 
 btns.forEach(btn =>{
   btn.addEventListener("click", (e) => {
@@ -103,16 +183,19 @@ btns.forEach(btn =>{
 
     if (e.currentTarget.id != "todos") {
       const titleCategory = products.find(product =>
-         e.currentTarget.id === product.id);
+      e.currentTarget.id === product.id);
       title.innerText = titleCategory.id;
 
       const productosFiltrados = products.filter(product =>
-         product.id === e.currentTarget.id);
-      uploadProducts(productosFiltrados)
+        product.id === e.currentTarget.id);
+        uploadProducts(productosFiltrados)
     }
     else{
-      uploadProducts(products)
-      title.innerText = "TODOS NUESTROS PRODUCTOS"
+        uploadProducts(productosFiltradosEnTodos)
+        title.innerText = "TODOS NUESTROS PRODUCTOS"
     }
   })
-})
+}
+)
+}
+uploadProducts(productosFiltradosEnTodos)
